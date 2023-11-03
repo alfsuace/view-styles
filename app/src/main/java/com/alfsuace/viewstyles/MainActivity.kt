09542b0182698a43dcb0025e3ef6698c67eebf63
alfsuace <alfonso.suaace@educa.jcyl.es>
2023-11-03 15:55:21 +0100

    project start

---
+++ app/src/main/java/com/alfsuace/viewstyles/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.alfsuace.viewstyles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.view_discounts)
+    }
+}
